31c5a57c5fedc7217216d2df8fe11d66f363eb52
plainswalker <plainswalker615@gmail.com>
2017-06-03 13:05:13 +0900

    commit : ModelInterface rev.

--- src/model/ModelInterface.java
+++ src/model/ModelInterface.java
@@ -16,28 +16,34 @@ public interface ModelInterface {
 	public Item loadLeft(String path);
 	public Item loadRight(String path);
 //}
+	public Item save(int lr);
+	public Item save(String data);
 	public Item save(List<String> data, int lr);
 //{	
 	// public Item saveLeft(List<String> date);
 	// public Item saveRight(List<String> date);
 //}
 
+	public Item edit(String data, int lr);
+	public Item edit(List<String> data, int lr);
 //{	
 	public Item edit(String line, int idx, int lr);
 	public Item editLeft(String line, int idx);
 	public Item editRight(String line, int idx);
 //}
 	
-	public Item compare(List<String> chData/*, int lr*/);
-	public Item compare(/*int lr*/);
+	
+	public Item compare();
+	//public Item compare(List<String> chData);
 
 	public Item getCompareResult(int lr);
 //{
 	public Item getCompareResultLeft();
 	public Item getCompareResultRight();
 //}
-	public Item merge(List<Integer> idxList, int lr);
+	
 	public Item merge(int idx, int lr);
 	public Item merge(int lr);
+	//public Item merge(List<Integer> idxList, int lr);
 }
 
