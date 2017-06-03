a96cdb210133aaab83dc13510f61a610633c709e
plainswalker <plainswalker615@gmail.com>
2017-06-03 12:51:27 +0900

    commit : ModelInterface

--- src/model/ModelInterface.java
+++ src/model/ModelInterface.java
@@ -1,19 +1,43 @@
 package model;
 
-import view.UI.DataSet.Item;
+import view.UI.DataSet.Item;//`ll be changed
+import view.UI.Position;//`ll be changed // can be enum class
 import java.util.List;
+import java.io.File;
 
 public interface ModelInterface {
-	public Item load(String path, boolean lr); // false : left, true : right
+	public Item load(File f, int lr);
+//{	
+	public Item loadLeft(File f);
+	public Item loadRight(File f);
+//}
+//{
+	public Item load(String path, int lr);
 	public Item loadLeft(String path);
 	public Item loadRight(String path);
+//}
+	public Item save(List<String> data, int lr);
+//{	
+	// public Item saveLeft(List<String> date);
+	// public Item saveRight(List<String> date);
+//}
 
-	public Item save(List<String> data, boolean lr);
-	public Item saveLeft(List<String> date);
-	public Item saveRight(List<String> date);
+//{	
+	public Item edit(String line, int idx, int lr);
+	public Item editLeft(String line, int idx);
+	public Item editRight(String line, int idx);
+//}
+	
+	public Item compare(List<String> chData/*, int lr*/);
+	public Item compare(/*int lr*/);
 
-	public Item compare();
-	public Item compareResultLeft();
-	public Item compareResultRight();
+	public Item getCompareResult(int lr);
+//{
+	public Item getCompareResultLeft();
+	public Item getCompareResultRight();
+//}
+	public Item merge(List<Integer> idxList, int lr);
+	public Item merge(int idx, int lr);
+	public Item merge(int lr);
 }
 
