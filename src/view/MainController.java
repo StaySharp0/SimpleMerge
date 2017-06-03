37bdafd20f6d94365bbd4f6b83377f9a56e56410
YongJun Kim <yongjunkim@YongJunui-MacBook-Pro.local>
2017-06-03 13:07:18 +0900

    Updae: EditUI

--- src/view/MainController.java
+++ src/view/MainController.java
@@ -1,10 +1,12 @@
 package view;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import javafx.scene.input.MouseEvent;
 import view.UI.Position;
 import view.UI.DataSet.Item;
 import view.UI.EditorUI;
@@ -29,7 +31,7 @@ public class MainController implements Initializable {
     @FXML private TextField fieldLeftFile, fieldRightFile;
     @FXML private TabPane tabLeft, tabRight;
 
-    @FXML private ListView<String> listLeft, listRight;
+
     @FXML private TextArea textAreaLeft,textAreaRight;
 
 
@@ -49,6 +51,10 @@ public class MainController implements Initializable {
     private Boolean condCompare = false;
 
 
+    @FXML private ListView<String> listLeft, listRight;
+    ObservableList<String>[] listModel = new ObservableList[2];
+
+
 
 
     @Override
@@ -66,17 +72,27 @@ public class MainController implements Initializable {
 
         button = new HashMap();
 
+        // init UI
         fileOpenUI[Position.LEFT] = new FileOpenUI(Position.LEFT, Main.getPrimaryStage(), fieldLeftFile, listLeft, textAreaLeft);
         fileOpenUI[Position.RIGHT] = new FileOpenUI(Position.RIGHT, Main.getPrimaryStage(), fieldRightFile, listRight, textAreaRight);
-        editorUI[Position.LEFT] = new EditorUI(Position.LEFT, tabLeft);
-        editorUI[Position.RIGHT] = new EditorUI(Position.RIGHT, tabRight);
+        editorUI[Position.LEFT] = new EditorUI(Position.LEFT, tabLeft, textAreaLeft, btnLeftFileEdit);
+        editorUI[Position.RIGHT] = new EditorUI(Position.RIGHT, tabRight, textAreaRight, btnRightFileEdit);
 
 
+        // set UI
         button.put(btnLeftFileOpen.getId(), fileOpenUI[Position.LEFT]);
         button.put(btnRightFileOpen.getId(), fileOpenUI[Position.RIGHT]);
-//        button.put(btnLeftFileEdit.getId(), editorUI[Position.LEFT]);
-//        button.put(btnRightFileEdit.getId(), editorUI[Position.RIGHT]);
+        button.put(btnLeftFileEdit.getId(), editorUI[Position.LEFT]);
+        button.put(btnRightFileEdit.getId(), editorUI[Position.RIGHT]);
+//        button.put(btnLeftFileEdit.getId(), editorUI[P₩Position.RIGHT]);
 
+
+
+        /* Set list model */
+        listModel[Position.LEFT] = listLeft.getItems();
+        listModel[Position.RIGHT] = listRight.getItems();
+
+        DoTest();
     }
 
     public void eventFileOpen(ActionEvent e) {
@@ -89,14 +105,14 @@ public class MainController implements Initializable {
         Boolean status = ui.onAction((source) -> {
 
             System.out.println("FileOpen callback");
-            // FileOpen data = Model.Update((File)source));
+            // FileOpen data = Model.load((File)source));
 
             return new Item();
         });
 
 
         int pos = ui.getPosition();
-        editorUI[pos].ShowList();
+        editorUI[pos].ShowViewMode();
         condLoadFile[pos] = status;
 
         // Set button disable
@@ -107,15 +123,39 @@ public class MainController implements Initializable {
 
     public void eventFileEdit(ActionEvent e) {
 
+        // Get Button
         Control target = (Control)e.getSource();
-//        button.get(target.getId()).onAction(()->{
-//
-//            return new Item();
-//        });
+        btnAction ui = button.get(target.getId());
+
+        // Call event
+        Boolean status = ui.onAction((source) -> {
+
+            System.out.println("FileEdit callback");
+            // FileOpen data = Model.Update((File)source));
+
+            return new Item();
+        });
     }
 
     public void eventFileSave(ActionEvent e) {
         System.out.println("Click:LeftFileSave");
     }
 
+
+    public void handleMouseClick(MouseEvent arg0) {
+//        System.out.println("clicked on " + listView.getSelectionModel().getSelectedItem());
+
+
+
+    }
+
+    public void DoTest(){
+        ObservableList<String> left = listLeft.getItems();
+        ObservableList<String> right = listRight.getItems();
+
+        left.setAll("1","2","3","4","1","2","3","4","1","2","3","4","1","2","3","4","1","2","3","4");
+        right.setAll("1","2","3","4","1","2","3","4","1","2","3","4","1","2","3","4");
+
+
+    }
 }
--- src/view/UI/EditorUI.java
+++ src/view/UI/EditorUI.java
@@ -1,6 +1,8 @@
 package view.UI;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
 
 /**
  * Created by yongjunkim on 2017. 5. 27..
@@ -8,22 +10,40 @@ import javafx.scene.control.TabPane;
 enum Status { VIEW, EDIT };
 
 public class EditorUI implements btnAction {
+    private Button btnEdit;
     private int position;
+    private TextArea textArea;
     private TabPane tabPane;
     private Status status;
 
-    public EditorUI(int pos, TabPane tabPane){
+    public EditorUI(int pos, TabPane tabPane, TextArea textArea, Button btnEdit){
         this.position = pos;
         this.tabPane = tabPane;
+        this.textArea = textArea;
+        this.btnEdit = btnEdit;
         this.status = status.VIEW;
     }
 
-    public void ShowList(){
-        tabPane.getSelectionModel().select(0);
+    public void ShowViewMode(){
+        status = status.VIEW;
+        textArea.setEditable(false);
+        tabPane.getSelectionModel().select(1);
+
+        btnEdit.getStyleClass().remove("edit");
+        btnEdit.getStyleClass().add("view");
+
+    }
+    public void ShowEditMode(){
+        status = status.EDIT;
+        textArea.setEditable(true);
+        tabPane.getSelectionModel().select(1);
+
+        btnEdit.getStyleClass().remove("view");
+        btnEdit.getStyleClass().add("edit");
     }
 
-    public void ShowTextArea(){
-        tabPane.getSelectionModel().select(1);
+    public void ShowCompareMode(){
+        tabPane.getSelectionModel().select(0);
     }
 
 
@@ -36,14 +56,13 @@ public class EditorUI implements btnAction {
     public Boolean onAction(EventCallback cb) {
         switch (status){
             case VIEW: // view -> edit
-                this.ShowTextArea();
-                this.status = status.EDIT;
-
+                ShowEditMode();
                 break;
             case EDIT: // edit -> view
-                this.ShowList();
-                this.status = status.VIEW;
+                ShowViewMode();
         }
+
+        cb.callbackMethod(textArea.getText());
 
         return true;
     }
