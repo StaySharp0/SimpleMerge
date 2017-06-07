4eea05419487d91549550ca799b81724027244fa
plainswalker <plainswalker615@gmail.com>
2017-06-08 02:34:43 +0900

    update : fix compare

--- src/model/Model.java
+++ src/model/Model.java
@@ -290,8 +290,8 @@ public class Model implements ModelInterface{
 
 
 		String[][] ListData = {
-				this.getResultLeft().toArray(new String[this.getResultLeft().size() + 1]),
-				this.getResultRight().toArray(new String[this.getResultRight().size() + 1])
+				this.getResultLeft().toArray(new String[this.getResultLeft().size()]),
+				this.getResultRight().toArray(new String[this.getResultRight().size()])
 		};
 
 		rtn.setListViewItem(ListData);
@@ -405,7 +405,7 @@ public class Model implements ModelInterface{
 
 			if(this.algo.lenLcs() > 0 && i == same.get(cntSame)){
 				buf.append(this.right.getLines().get(same.get(cntSame)));
-				if(i + 1 < this.right.length() || (!this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin)){
+				if(i + 1 < this.right.length() || (!this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin))	{
 					buf.append("\n");
 					if(cntSame < same.size()){
 						cntSame++;
