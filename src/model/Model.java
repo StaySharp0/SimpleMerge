3e3f2c13a834a6b41bdad9d34b3357f70e071a09
plainswalker <plainswalker615@gmail.com>
2017-06-08 02:25:00 +0900

    update : fix compare

--- src/model/Model.java
+++ src/model/Model.java
@@ -292,7 +292,6 @@ public class Model implements ModelInterface{
 		String[][] ListData = {
 				this.getResultLeft().toArray(new String[this.getResultLeft().size() + 1]),
 				this.getResultRight().toArray(new String[this.getResultRight().size() + 1])
-
 		};
 
 		rtn.setListViewItem(ListData);
@@ -352,7 +351,7 @@ public class Model implements ModelInterface{
 
 			if(this.algo.lenLcs() > 0 && i == same.get(cntSame)){
 				buf.append(this.left.getLines().get(same.get(cntSame)));
-				if(i + 1 < this.left.length() && !this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin){
+				if(i + 1 < this.left.length() || (!this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin)){
 					buf.append("\n");
 					if(cntSame < same.size()){
 						cntSame++;
@@ -406,7 +405,7 @@ public class Model implements ModelInterface{
 
 			if(this.algo.lenLcs() > 0 && i == same.get(cntSame)){
 				buf.append(this.right.getLines().get(same.get(cntSame)));
-				if(i + 1 < this.right.length() && !this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin){
+				if(i + 1 < this.right.length() || (!this.algo.isIdentical() &&  i + 1 < diff.get(cntDiff).begin)){
 					buf.append("\n");
 					if(cntSame < same.size()){
 						cntSame++;
